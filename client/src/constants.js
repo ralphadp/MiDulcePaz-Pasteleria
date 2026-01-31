d40614ad19128f3a02792ae2f9aa0eecf63e2fa3
ralph.asendeteufrer <ralphadp@gmail.com>
2026-01-31 05:10:00 -0400

    Adding env vars

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 // constants.js
-export const API_BASE_URL = 'http://localhost:8080/';
+export const API_BASE_URL = process.env.REACT_APP_API_SERVER_HOST;
 export const APP_VERSION = '1.0.0';
-export const APP_BASE_URLPORT = '8080';
+export const APP_BASE_URLPORT = process.env.REACT_APP_API_SERVER_PORT;
 
 export const API_STANDARD = API_BASE_URL + 'standard';
 export const API_PRODUCTS = API_BASE_URL + 'products';
